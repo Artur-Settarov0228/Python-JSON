c70977a53107f30b93b5a07eec10bc13e2d083fb
settarovartur <settarovartur2@gamil.com>
2025-07-03 19:51:33 +0500

    bajarildi

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,1 +1,9 @@
+import json
 
+with open('students.json' , 'r') as json_file:
+    students = json.load(json_file)
+
+sorted_students=students.sort(key=lambda x : x['score'] , reverse = True)
+
+with open('rating.json' ,'w') as json_file_w:
+    json.dump(students , json_file_w, indent=4)
